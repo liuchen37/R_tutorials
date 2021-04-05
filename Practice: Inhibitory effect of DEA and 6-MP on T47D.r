ff50f623056918b2e3ff329df7729ba1e767ee43
Chen Liu <liuchen37@gmail.com>
2021-04-05 21:48:57 +0100

    Update Practice: Inhibitory effect of DEA and 6-MP on T47D.r

--- Practice: Inhibitory effect of DEA and 6-MP on T47D.r
+++ Practice: Inhibitory effect of DEA and 6-MP on T47D.r
@@ -19,3 +19,19 @@ ggplot(DEA24, aes(x = Dosage, y = Absorbance, colour = Drug)) +
   theme(text = element_text(family = "Times New Roman")) +
   scale_x_continuous(limits = c(0,1000), breaks=seq(0,1000,200)) +
   ggsave("4a.png")
+
+#Compare means
+D200 <- filter(DEA24, Dosage == 200)
+D200 %>%
+  group_by(Drug) %>%
+  summarise(meanAbs200 = mean(Absorbance))
+
+D600 <- filter(DEA24, Dosage == 600)
+D600 %>%
+  group_by(Drug) %>%
+  summarise(meanAbs600 = mean(Absorbance))
+
+D1000 <- filter(DEA24, Dosage == 1000)
+D1000 %>%
+  group_by(Drug) %>%
+  summarise(meanAbs1000 = mean(Absorbance))
