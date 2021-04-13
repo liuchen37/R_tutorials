c86027f19dec4f57c6cedc511ddb15f30aa851b9
Chen Liu <liuchen37@gmail.com>
2021-04-13 22:18:48 +0100

    Update Foundation: Visualising data with ggplot2 II.r

--- Foundation: Visualising data with ggplot2 II.r
+++ Foundation: Visualising data with ggplot2 II.r
@@ -1,1 +1,14 @@
 # Visualising data with ggplot2 II, Chen
+#Demo data: compensation.csv
+
+#Install gridExtra and load files
+rm(list = ls())
+library(ggplot2)
+install.packages("gridExtra")
+library(gridExtra)
+
+file.choose()
+compensation <- read.csv("file path)
+compensation
+
+#Assign plots
