698c89145e1184256c5b8ca57bdaa2ac20363343
Chen Liu <liuchen37@gmail.com>
2021-04-13 22:29:20 +0100

    Update Foundation: Visualising data with ggplot2 II.r

--- Foundation: Visualising data with ggplot2 II.r
+++ Foundation: Visualising data with ggplot2 II.r
@@ -10,3 +10,26 @@ library(gridExtra)
 file.choose()
 compensation <- read.csv("file path)
 compensation
+
+#Assign plots
+egscatter <-
+  ggplot(compensation, aes(x = Root, y = Fruit)) +
+  geom_point()
+
+egbox <-
+  ggplot(compensation, aes(x = Root, y = Fruit)) +
+  geom_boxplot()
+
+#Add lines to existing assignment
+egscatter + theme_bw()
+
+#Arrangements
+grid.arrange(egscatter, egbox, nrow= 1)
+grid.arrange(egscatter, egbox, ncol= 1)
+
+#Title
+egscatter + ggtitle("Fruit production vs Root length")
+
+#Axis limits
+egscatter + xlim (6,8) + ylim(60 , 90)
+
