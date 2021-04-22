0f96b68cc71300a8fef8dbdcca002d9cc7398e79
Chen Liu <liuchen37@gmail.com>
2021-04-22 16:02:02 +0100

    Update Quantitative RNAseq: Estimating differential expression.r

--- Quantitative RNAseq: Estimating differential expression.r
+++ Quantitative RNAseq: Estimating differential expression.r
@@ -32,7 +32,17 @@ columns_of_interest <- which(pheno[['stage']] %in% experiments_of_interest)
 #Piping method to send columns of interest to group factor
 grouping <- pheno[['stage']][columns_of_interest] %>% 
   forcats::as_factor()
+#Form count data subset
+counts.of.interest <- count.matrix[columns_of_interest]
 
-#
+#Create the DGE project
+DGE <- edgeR::DGEList(counts = counts.of.interest, group = grouping)
+
+#Differential expression analysis
+desgin <- model.matrix(~ grouping)
+eset_dge <- edgeR::estimateCommonDisp(eset_dge, design)
+fit <- edgeR::glmQLFit(eset_dge, design)
+results <- edgeR::glmQLFTest(fit, coef = 2)
+topTags(results)
 
 #1-1-2: with DESeq2
