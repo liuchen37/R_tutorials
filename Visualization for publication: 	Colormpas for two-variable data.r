b5bb7f8f372b5f243074a5b100470f893b616f01
Chen Liu <liuchen37@gmail.com>
2021-04-28 15:53:38 +0100

    Update Visualization for publication: 	Colormpas for two-variable data.r

--- Visualization for publication: 	Colormpas for two-variable data.r
+++ Visualization for publication: 	Colormpas for two-variable data.r
@@ -1,2 +1,18 @@
 #7.2 	Colormpas for two-variable data
 #Demo data: WorldPhones, built-in with R
+
+rm(list = ls())
+wp <- WorldPhones
+
+#Contrast a heatmap
+heatmap(wp)
+
+#Remove the dendrogram
+heatmap(wp, Rowv = NA, Colv = NA)
+
+#Add colour scale to the groups
+cc <- rainbow(ncol(wp), start = 0, end = 0.3)
+heatmap(wp, ColSideColors = cc)
+
+#Rescale
+heatmap(wp, ColSideColors = cc, scale = "column")
